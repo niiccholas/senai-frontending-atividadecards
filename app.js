732d5ea6176232dec0ecee1dba54d35efd352cb9
NICOLAS SILVA DE ALMEIDA SANTOS <24122316@CFP127ALUNO.LOCAL>
2025-02-06 15:58:47 -0300

    json feito

---
+++ app.js
@@ -0,0 +1,8 @@
+músicas = [
+    {nome: 'Sangue', album: 'Ordem Paranormal: Desconjuração', autor: 'JuVi', cor: '#d91709', imagem: 'OPD.webp'},
+    {nome: 'Detetives do Condomínio Azul', album: 'Natal Macabro', autor: 'Schiavoto', cor: '#99050a', imagem: 'OPNM.webp'},
+    {nome: 'Quarentena (Abertura)', album: 'Ordem Paranormal: Quarentena', autor:'JuVi', cor: '#803e9e', imagem: 'OPQ.webp'},
+    {nome: 'Iniciativa', album: 'Ordem Paranormal: Calamidade', autor: 'Juvi', cor:'#241c00', imagem: 'OPC.webp'}
+]
+
+
