136acf9895cb86f487f3500a0f995366bbf90b07
NICOLAS SILVA DE ALMEIDA SANTOS <24122316@CFP127ALUNO.LOCAL>
2025-02-13 14:49:57 -0300

    correções pra adaptação de novos itens

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ musicas = [
     {nome: 'Sangue', autor: 'JuVi', cor: '#d91709', imagem: 'OPD.webp', link: 'https://www.youtube.com/watch?v=uMkRnoXh2AY'},
     {nome: 'Detetives do Condomínio Azul', autor: 'Schiavoto', cor: '#99050a', imagem: 'OPNM.webp', link: 'https://www.youtube.com/watch?v=ioJ47bD0-pM'},
     {nome: 'Quarentena (Abertura)', autor:'JuVi', cor: '#803e9e', imagem: 'OPQ.webp', link: 'https://www.youtube.com/watch?v=UDVSM935dtY'},
-    {nome: 'Iniciativa', autor: 'Juvi', cor:'#241c00', imagem: 'OPC.webp', link: 'https://www.youtube.com/watch?v=IWC4axoJbpw'}
+    {nome: 'Iniciativa', autor: 'Juvi', cor:'#241c00', imagem: 'OPC.webp', link: 'https://www.youtube.com/watch?v=IWC4axoJbpw'},
+
 ]
 
 function criarCard (musica){
     const novoItem = document.createElement('li')
     const novaImagem = document.createElement('img')
+
     const novoTitulo = document.createElement('h2')
+    const container = document.createElement('div')
     const novoSubtitulo = document.createElement('h3')
     const novoContainer = document.createElement('div')
     const novoLink = document.createElement('a')
     const listaCards = document.getElementById('menu')
 
+    novoContainer.classList.add('link')
     novaImagem.src = ('./img/' + musica.imagem)
     novoTitulo.textContent = musica.nome
     novoSubtitulo.textContent = musica.autor
@@ -22,9 +26,10 @@ function criarCard (musica){
 
     novoContainer.appendChild(novoLink)
     novoItem.appendChild(novaImagem)
-    novoItem.appendChild(novoTitulo)
-    novoItem.appendChild(novoSubtitulo)
-    novoItem.appendChild(novoContainer)
+    container.appendChild(novoTitulo)
+    container.appendChild(novoSubtitulo)
+    container.appendChild(novoContainer)
+    novoItem.appendChild(container)
 
     novoContainer.style = `--cor-hover: ${musica.cor}`
 
